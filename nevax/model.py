7b4a14ce15f7e9d0c318163cdc2d6996dbdbab4f
Kye <kye@apacmediasolutions.com>
2023-08-17 20:41:33 -0400

    pretraind

--- nevax/model.py
+++ nevax/model.py
@@ -1,7 +1,12 @@
-
+import logging
 import torch
 import torch.nn as nn
-from transformers import AutoTokenizer, CLIPProcessor
+from transformers import AutoTokenizer, CLIPProcessor, CLIPModel
+
+from flamingo_pytorch import PerceiverResampler
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
 
 from nevax.transformer import (
     AndromedaEmbedding,
@@ -13,9 +18,93 @@ from nevax.transformer import (
 )
 
 
+# Implement classes with type hints and error handling
+class NevaTokenizer:
+    """
+    A tokenizer class for the Neva model
+
+    Attributes:
+        processor(CLIPProcessor): The processor to tokenize images
+        tokenizer: (AutoTokenizer): The tokenizer to tokenize text 
+        im_idx: (int): The Index of the "<image>" token.
+        im_end_idx (int): The index of the "</image>" token.
+    """
+
+    def __init__(self):
+        try:
+            self.processor = CLIPProcessor.from_pretrained("laion/CLIP-ViT-L-14-laion2B-s32B-b82K")
+            self.tokenizer = AutoTokenizer.from_pretrained(
+                "EleutherAI/gpt-neox-20b",
+                additional_special_tokens=["<image>", "</image>"],
+                eos_token="<eos>",
+                pad_token="<pad>",
+                extra_ids=0,
+                model_max_length=8192
+            )
+        except Exception as e:
+            logging.error(f"Failed to initialize NevaTokenizer: {e}")
+            raise
+
+        self.im_idx, self.im_end_idx = self.tokenizer.convert_tokens_to_ids(["<image>", "</image>"])
+
+    def tokenize_texts(self, texts: str):
+        """
+        Tokenize given texts.
+        Args:
+            Texts (str): The Text to be tokenized
+        Returns:
+            A tuple containing the tokenized texts and only the text tokens.
+        """
+        try:
+            texts =  self.tokenizer(texts, return_tensors="pt", padding=True, truncation=True).input_ids
+            # Add image tokens to text as "<s> <image> </image> text </s>"
+            image_tokens = torch.tensor([[self.im_idx, self.im_end_idx]] * texts.shape[0])
+            return torch.cat([texts[:, 0:1], image_tokens, texts[:, 1:]], dim=1), texts
+        except Exception as e:
+            logging.error(f"Failed to tokenize texts: {e}")
+            raise
+
+    def tokenize_images(self, images):
+        """
+        Tokenizes given images.
+        Args:
+            images: The images to be tokenized
+        Returns:
+            The tokenized images.
+        """
+        try:
+            return self.processor(images=images, return_tensors="pt").pixel_values
+        except Exception as e:
+            logging.error(f"Failed to tokenize images: {e}")
+            raise
+
+    def tokenize(self, sample):
+        """
+        Tokenizes given sample.
+        Args:
+            Sample: The sample to be tokenized
+        Returns:
+            A dictionary containing the tokenized text tokens, images, labels, and attention mask.
+        """
+        try:
+            text_tokens, only_text_tokens = self.tokenize_texts(sample["target_text"])
+            attention_mask = text_tokens != self.tokenizer.pad_token_id
+            dummy_image_features = torch.ones((text_tokens.shape[0], 64))
+            attention_mask = torch.cat([dummy_image_features, attention_mask], dim=1)
+            return {
+                "text_tokens": text_tokens,
+                "images": self.tokenize_images(sample["image"]),
+                "labels": only_text_tokens,
+                "attention_mask": attention_mask,
+            }
+        except Exception as e:
+            logging.error(f"Failed to tokenize sample: {e}")
+            raise
+
+
 class Neva(nn.Module):
     """
-    Andromeda is a transformer-based model architecture. It initializes with 
+    Neva is a transformer-based model architecture. It initializes with 
     a Transformer and AutoregressiveWrapper with default or user-specified parameters.
 
     Initialize the model with specified or default parameters.
@@ -60,16 +149,34 @@ class Neva(nn.Module):
                  embedding_provider=AndromedaEmbedding()):
         super(Neva).__init__()
 
-        self.encoder = ViTransformerWrapper(
-            image_size=image_size,
-            patch_size=patch_size,
-            attn_layers=Encoder(
-                dim=dim,
-                depth=depth,
-                dim_head=dim_head,
-                heads=heads
-            )
+        # self.encoder = ViTransformerWrapper(
+        #     image_size=image_size,
+        #     patch_size=patch_size,
+        #     attn_layers=Encoder(
+        #         dim=dim,
+        #         depth=depth,
+        #         dim_head=dim_head,
+        #         heads=heads
+        #     )
+        # )
+
+        self.clip_model = CLIPModel.from_pretrained("laion/CLIP-ViT-L-14-laion2B-s32B-b82K").vision_model
+        self.output_projection = nn.Linear(2048, num_tokens, bias=False)
+        nn.init.normal_(self.output_projection.weight, mean=0, std=2048**-0.5)
+
+        self.perceive = PerceiverResampler(
+            dim=1024,
+            depth=2,
+            dim_head=64,
+            heads=8,
+            num_latents=64,
+            num_media_embeds=257
         )
+
+        self.image_proj = nn.Linear(1024, dim, bias=False)
+        nn.init.normal_(self.image_proj.weight, mean=0, std=dim**-0.5)
+        
+
 
         self.transformer = Transformer(
             num_tokens=num_tokens,
@@ -105,6 +212,10 @@ class Neva(nn.Module):
         - output from the decoder
         """
         try:
+            images = self.clip_model(pixel_values=img)["last_hidden_state"]
+            images = self.perceive(images).squeeze(1)
+            images = self.image_proj(images)
+            
             encoded_img = self.encoder(img, return_embeddings=True)
             return self.decoder(text_tokens, context=encoded_img)
         except Exception as error:
